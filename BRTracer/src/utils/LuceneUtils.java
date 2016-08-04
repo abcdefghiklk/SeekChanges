521457c2bcdc70eaa879de1c3d0e68870f4843cd
Qiuchi Li <Qiuchi.Li@open.ac.uk>
2016-08-04 11:38:58 +0100

    Adding reference libraries in the BRTracer\lib

---
+++ BRTracer/src/utils/LuceneUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.apache.lucene.search.similarities.TFIDFSimilarity;
+
+public class LuceneUtils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		TFIDFSimilarity s=new();
+		
+	}
+
+}
